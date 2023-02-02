d3ed8cafb45611636a3029d1fb9a34c4ea37d28f
JaviPeder~ <javierpedernera@gmail.com>
2023-02-02 19:23:04 -0300

    prueba

---
+++ api/src/controllers/newProduct.js
@@ -0,0 +1,55 @@
+const { Wine } = require("../db");
+
+const newProduct = async function (name, price, image, volumen, quantity, category, stock, details, grapesID, statesID, regionsID, typeID) {
+  //Busco wine por name para saber si existe
+//   grapesId?
+  if (!name || !price || !image || !volumen || !quantity || !category || !stock || !details || !grapesID || !statesID || !regionsID || !typeID ) {
+    throw new Error('Debe completar todos los campos requeridos')
+  }
+  const searchWine = await Wine.findOne({
+    where: {
+      name: name,
+    },
+  });
+  // console.log(searchWine)
+  // busco el pais que coincida con id
+//   const grapesMatch = await Grapes.findAll({
+//     where: {
+//       id: grapesID,
+//     },
+//   });
+  // console.log(grapesMatch)
+//   if (grapesMatch.length === 0) {
+//     throw new Error(`No se encuentra el país ${countryID} para agregar actividad`)
+//   }
+  // si no existe en la base de datos, crearla
+  !volumen || !grapes || !quantity || !stock || !details || !winerys ||  !image || !states || !regions
+  if (!searchWine) {
+    const newWine = await Wine.create({
+      name: name,
+      price: price,
+      volumen: volumen,
+
+      grapes: grapes,
+    //   ?
+      quantity: quantity,
+      stock: stock,
+      details: details,
+      winerys: winerys,
+      image: image,
+      states: states,
+      regions: regions
+    });
+    //agrego la el vino a las cepas?
+    // await newWine.addGrapes(grapesMatch);
+
+    return `New box ${name} was created and added successfully`
+  } else {
+    // console.log(countrymatch)
+    // const ActivityAdd = 
+    await searchWine.addGrapes(grapesMatch);
+    // return ActivityAdd;
+    return `Box ${name} added successfully`
+  }
+}
+module.exports = { newProduct };
---
+++ api/src/routes/products.js
@@ -0,0 +1,60 @@
+const { Router } = require('express');
+const { newProduct } = require('../controllers/newProduct')
+const { wineById } = require('../controllers/wineById')
+const { deleteWine } = require('../controllers/deleteWine')
+const { updateWine } = require('../controllers/updateWine')
+// const { Wine } = require("../db");
+const router= Router();
+
+router.post('/', async (req, res)=>{
+    try {
+    const { name, price, image, volumen, quantity, category, stock, details, grapesID, statesID, regionsID, typeID } = req.body;
+    let result = await newWine( name, price, image, volumen, quantity, category, stock, details, grapesID, statesID, regionsID, typeID )
+    res.status(200).send({result})
+} catch (error) {
+    res.status(400).send(error.message)
+}
+})  
+
+router.get('/', async (req, res) => {
+    try {
+        let { name } = req.query;
+        const result = await getWine(name);
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+});
+
+router.get('/:idWine', async (req, res) => {
+    const productId = req.params.idWine
+    try {
+        let result = await productById(productId);
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+});
+
+router.put('/:id', async (req, res)=>{
+    try {
+        const id = req.params.id
+        const { name, price, image, volumen, quantity, category, stock, details, grapesID, statesID, regionsID, typeID } = req.body;
+    let result = await updateWine(id, name, price, image, volumen, quantity, category, stock, details, grapesID, statesID, regionsID, typeID)
+    res.status(200).send(result)
+} catch (error) {
+    res.status(400).send(error.message)
+}
+})
+
+router.delete('/:id', async (req, res)=>{
+    try {
+        const id = req.params.id
+    let result = await deleteWine(id)
+    res.status(200).send(result)
+} catch (error) {
+    res.status(400).send(error.message)
+}
+})  
+
+module.exports = router;
