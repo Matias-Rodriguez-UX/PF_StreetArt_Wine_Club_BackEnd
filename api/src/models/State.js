402249c3e4d40ddd7b845f9154b0f22474b97076
paualarcon7 <alarconayelenpaulina@gmail.com>
2023-02-02 17:02:30 -0300

    feat(api/src): add states and regions models

---
+++ api/src/models/State.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "state",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
+};
