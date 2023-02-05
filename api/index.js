5bde73d65896c57d4a2afe38ae308938225a8dd1
JaviPeder~ <javierpedernera@gmail.com>
2023-02-05 13:49:57 -0300

    Feat(api/src/controllers/getProducts) getProducts conexion tablas intermedias

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 const {LoadingDb} = require('./src/DbLoad.js')
 const {LoadingProducts} = require('./src/DbLoadProducts.js')
 // Sincronización de todos los modelos:
-conn.sync({ force: true}).then(() => {
+conn.sync({ force: false}).then(() => {
   server.listen(3001, () => {
     LoadingDb()
     LoadingProducts()
