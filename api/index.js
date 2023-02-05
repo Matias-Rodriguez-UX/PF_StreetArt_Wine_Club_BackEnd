21145873bd8089a3e8fe6252e2401021b285be98
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-02-05 16:05:07 -0300

    back

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {LoadingDb} = require('./src/DbLoad.js')
 // Sincronización de todos los modelos:
-conn.sync({ force: false}).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     LoadingDb()
     console.log('%s listening at 3001');
