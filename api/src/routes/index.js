7b75ba4f6de276028d404224a4b52a579e7d6611
JaviPeder~ <javierpedernera@gmail.com>
2023-02-06 13:44:13 -0300

    Feat(api/src/routes/states) ruta states

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,13 +2,18 @@ const { Router } = require('express');
 // Importar todos los routers;
 const ProductRouter= require('./products')
 const GrapeRouter= require('./grapes')
-
+const StateRouter= require('./states')
+// const RegionRouter= require('./regions')
+// const TypeRouter= require('./types')
 
 const router = Router();
 
 // Configurar los routers
 router.use('/products', ProductRouter);
 router.use('/grapes', GrapeRouter);
+router.use('/states', StateRouter);
+// router.use('/regions', RegionRouter);
+// router.use('/types', TypeRouter);
 
 module.exports = router;
 
