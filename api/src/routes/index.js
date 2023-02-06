d4121128dfb458d8d094eb94dde9b9da7e01943f
JaviPeder~ <javierpedernera@gmail.com>
2023-02-06 07:37:15 -0300

    Feat(api/src/routes/grapes) get all grapes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 // Importar todos los routers;
 const ProductRouter= require('./products')
+const GrapeRouter= require('./grapes')
 
 
 const router = Router();
 
 // Configurar los routers
 router.use('/products', ProductRouter);
-
+router.use('/grapes', GrapeRouter);
 
 module.exports = router;
 
