1d3365503be0b6c5e192adeca5f1abfcad007d8b
JaviPeder~ <javierpedernera@gmail.com>
2023-02-10 18:13:03 -0300

    Feat(api/src/routes/orders) ruta orders

--- api/src/controllers/postOrder.js
+++ api/src/controllers/postOrder.js
@@ -21,20 +21,3 @@ if(!orderNumber || !orderNumber || !totalPrice){
 module.exports = {newOrder}
 
 
-//  orderNumber: {
-//         type: DataTypes.STRING,
-//         primaryKey: true,
-//       },
-//       date: {
-//         type: DataTypes.DATEONLY,
-//         defaultValue: DataTypes.NOW
-//       },
-//       totalPrice: {
-//         type: DataTypes.FLOAT,
-//         allowNull: false,
-//       },
-//       status: {
-//         type: DataTypes.ENUM('processing payment', 'processing shipping', 'shipped', 'delivered', 'cancelled'),
-//         defaultValue: 'processing payment',
-//         allowNull: false,
-//       },
