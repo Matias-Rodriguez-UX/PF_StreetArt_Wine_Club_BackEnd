36df766300722ed5e1697648f09035caa9eb2e14
ValeBazan <mvaleriabzn@gmail.com>
2023-02-11 18:09:11 -0300

    memberships falta asociar al usuario

--- api/src/controllers/newMembership.js
+++ api/src/controllers/newMembership.js
@@ -1,25 +1,37 @@
 const { Membership, User } = require("../db");
 
-const newMembership = async function(id, name, discount, price ){
+const newMembership = async function( id , name, discount, price ){
     
-    if(!name || !discount || !price){
-        throw new Error('The fields are required')
-    }
+    // if(!name || !discount || !price){
+    //     throw new Error('The fields are required')
+    // }
     
-        const addMembership = await Membership.create({
-            id: id,
-            name: name,
-            discount: discount,
+    const searchMembership = await Membership.findOrCreate({
+        where: {
+            name : name,
+            discount : discount,
             price: price
-           
-            
-        })
+         
+        },
+    })
     
+    console.log('MEMBRESIA BUSCADA', searchMembership )
+
     
-        if(addMembership){
-            return 'The membership was created successfully'
-        }
-    
+    const searchUser =  await User.findAll({
+        where: {
+            id: id,
+        },
+    })
+    console.log(' el USUARIO', searchUser)
+
+    if(searchUser){
+        await searchMembership.setUser(searchMembership)
+        return "Se agrego el ID"
     }
+
+    }
+    
+    
     
     module.exports = {newMembership}
