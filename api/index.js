4114b00f2a8d459c29432128539753d8299d5a91
JaviPeder~ <javierpedernera@gmail.com>
2023-02-15 11:22:13 -0300

    agrego review a producto por id y algunos cambios para hacer el deploy

--- api/index.js
+++ api/index.js
@@ -3,11 +3,12 @@ const { conn } = require('./src/db.js');
 const {LoadingDb} = require('./src/DbLoad.js')
 const {LoadingProducts} = require('./src/DbLoadProducts.js')
 // Sincronización de todos los modelos:
+const port = process.env.PORT || 3001 
 
-conn.sync({ force: false}).then( () => {
-  server.listen(3001, async() => {
+conn.sync({ force: true}).then( () => {
+  server.listen(port, async() => {
     await LoadingDb()
     await LoadingProducts()  
-    console.log('%s listening at 3001');
+    console.log(`%s listening at ${port}`);
   });
 });
