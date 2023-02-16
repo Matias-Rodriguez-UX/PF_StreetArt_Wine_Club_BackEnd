dca3fec0bb4aeb01ab14849e24bb2a0e74cd9647
Matias Rodriguez <rodriguez.matias.g@gmail.com>
2023-02-16 18:56:07 -0300

    cambios back

--- api/index.js
+++ api/index.js
@@ -1,14 +1,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const {LoadingDb} = require('./src/DbLoad.js')
-const {LoadingProducts} = require('./src/DbLoadProducts.js')
+const { LoadingDb } = require('./src/DbLoad.js')
+const { LoadingProducts } = require('./src/DbLoadProducts.js')
 // Sincronización de todos los modelos:
-const port = process.env.PORT || 3001 
-
-conn.sync({ force: false}).then( () => {
-  server.listen(port, async() => {
+conn.sync({ force: false }).then(() => {
+  server.listen(3001, async () => {
     await LoadingDb()
-    await LoadingProducts()  
-    console.log(`%s listening at ${port}`);
+    await LoadingProducts()
+    console.log('%s listening at 3001');
   });
 });
