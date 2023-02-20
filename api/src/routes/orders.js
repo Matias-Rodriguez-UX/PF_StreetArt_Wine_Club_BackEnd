a842b691b6854ef9e2234d8d8b4ac62a03d32b57
paualarcon7 <alarconayelenpaulina@gmail.com>
2023-02-20 19:04:37 -0300

    (src/routes): orders.js agregamos /backToCart

--- api/src/routes/orders.js
+++ api/src/routes/orders.js
@@ -1,59 +1,75 @@
 const express = require("express");
 const router = express.Router();
-const { Product, Order, ShoppingCart, User } = require('../db.js');
-const { getOrderId } = require('../controllers/getOrderId');
-const {changeOrder} = require('../controllers/changeOrder');
+const { Product, Order, ShoppingCart, User } = require("../db.js");
+const { getOrderId } = require("../controllers/getOrderId");
+const { changeOrder } = require("../controllers/changeOrder");
 
+// ruta que retorna todas las ordenes(solo dar acceso Admin)
 
-    // ruta que retorna todas las ordenes(solo dar acceso Admin)
+router.get("/", async (req, res) => {
+  try {
+    let allOrders = await Order.findAll({
+      include: { model: User, as: "user" },
+    });
+    res.status(200).send(allOrders);
+  } catch (error) {
+    res.status(400).send("no purchase orders created");
+  }
+});
 
-router.get('/', async (req, res) => {
-    try {
-        let allOrders = await Order.findAll({ include: { model: User, as: 'user' } })
-        res.status(200).send(allOrders)
-    } catch (error) {
-        res.status(400).send('no purchase orders created')
-    }
-})
-
-    // retornar orden por id
-    router.get('/:id', async (req, res) => {
-        try {
-            const { id } = req.params;
-            let result = await getOrderId(id)
-            res.status(200).send(result)
-        } catch (error) {
-            res.status(400).send(error.message)
-        }
-    })
+// retornar orden por id
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    let result = await getOrderId(id);
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 //get order del user
-    router.get('/', async (req, res) => {
-        try {
-            const { email } = req.query;
-            let result = await Order.findAll({
-                where: {
-                    userEmail: email
-                }
-            })
-            res.status(200).send(result)
-        } catch (error) {
-            res.status(400).send(error.message)
-        }
-    })
+router.get("/", async (req, res) => {
+  try {
+    const { email } = req.query;
+    let result = await Order.findAll({
+      where: {
+        userEmail: email,
+      },
+    });
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 
-    
 // crear ruta para modificar una Orden
-router.put('/checkout', async (req, res, ) => {
-    try {
-        const { status, email } = req.body;
-        let result = await changeOrder( status, email  )
-        res.status(200).send(result)
-    } catch (error) {
-        res.status(400).send(error.message)
-    }
-})
+router.put("/checkout", async (req, res) => {
+  try {
+    const { status, email } = req.body;
+    let result = await changeOrder(status, email);
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 
-
-
+router.put("/backToCart/:orderId", async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    let result = await Order.update(
+      {
+        status: "cart",
+      },
+      {
+        where: {
+          id: orderId,
+        },
+      }
+    );
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 
 module.exports = router;
