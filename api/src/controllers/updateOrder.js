5f6bd31389264c6e8fdfde388d729e35e8616d24
Matias Rodriguez <rodriguez.matias.g@gmail.com>
2023-02-26 17:04:29 -0300

    change in orders

--- api/src/controllers/updateOrder.js
+++ api/src/controllers/updateOrder.js
@@ -1,6 +1,6 @@
 const { User, ShoppingCart, Order, Product, Address } = require("../db");
 
-const updateOrderById = async function(orderId, status, addressId){
+const updateOrderById = async function (orderId, status, addressId) {
 
     const searchOrder = await Order.findOne({
         where: {
@@ -8,43 +8,52 @@ const updateOrderById = async function(orderId, status, addressId){
         },
     });
 
-    if(searchOrder){
+    if (searchOrder) {
 
-        if(addressId !== null){
+        if (addressId !== null) {
             const searchAddress = await Address.findOne({
                 where: {
                     id: addressId,
                 },
             });
 
-            
-        const update = await Order.update(
-            { status: status, 
-              addressId: searchAddress.id,
-              id: orderId},
-            { where: {
-                status: searchOrder.status, 
-                addressId: searchOrder.addressId } },
-        )
-    
-        if (update){
-            return 'Your order status and address was successfully updated'
+            const update = await Order.update(
+                {
+                    status: status,
+                    addressId: searchAddress.id,
+                    id: orderId
+                },
+                {
+                    where: {
+                        status: searchOrder.status,
+                        addressId: searchOrder.addressId,
+                        id: searchOrder.id,
+                    }
+                },
+            )
+
+            if (update) {
+                return 'Your order status and address was successfully updated'
+            }
+
+        } else {
+            const updateStatus = await Order.update(
+                {
+                    status: status,
+                },
+                {
+                    where: {
+                        status: searchOrder.status,
+                        id: orderId
+                    }
+                },
+            )
+
+            if (updateStatus) {
+                return 'Your order status was successfully updated'
+            }
+
         }
-
-    } else {
-        const updateStatus = await Order.update(
-            { status: status, 
-             },
-            { where: {
-                status: searchOrder.status,
-                id: orderId } },
-        )
-
-        if (updateStatus){
-            return 'Your order status was successfully updated'
-        }
-
-    }
 
     }
 
@@ -52,4 +61,4 @@ const updateOrderById = async function(orderId, status, addressId){
 
 }
 
-module.exports = {updateOrderById}
+module.exports = { updateOrderById }
