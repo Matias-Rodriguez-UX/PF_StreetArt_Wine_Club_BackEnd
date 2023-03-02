91f25fb42eee219ed23176219e9ac5b716324e68
Juan Javier Pedernera <102430306+JaviPeder@users.noreply.github.com>
2023-03-02 20:32:28 -0300

    Update changeOrder.js

--- api/src/controllers/changeOrder.js
+++ api/src/controllers/changeOrder.js
@@ -100,7 +100,7 @@ const changeOrder = async function (status, email, orderId, addressId, newAddres
         }
       );
       //envio de mail confirmando la compra
-      //   await purchaseConfirmation(email, orderSelectId);
+        await purchaseConfirmation(email, orderSelectId);
       return updated;
     }
   } else if (orderId) {
@@ -125,7 +125,7 @@ const changeOrder = async function (status, email, orderId, addressId, newAddres
 
       //envia mail de compra realizada
 
-      //   await orderShipped(email, orderId)
+        await orderShipped(email, orderId)
       return `The order ${orderId} was updated successfully`;
     }
 
